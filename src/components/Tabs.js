f921fc0517dec68f8bea5be62ec3cf98e4e76225
Nikk45 <nicksin36@gmail.com>
2023-08-28 13:28:44 +0530

    submit 1

---
+++ src/components/Tabs.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function Tabs({ tabs, key }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div className="tabs">
+      <div className="tab-list">
+        {tabs.map((tab, index) => (
+          <div
+            key={index}
+            className={`tab ${activeTab === index ? 'active' : ''}`}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab.title}
+          </div>
+        ))}
+      </div>
+      <div className="tab-content">
+        {tabs[activeTab].content}
+      </div>
+    </div>
+  );
+}
+
+export default Tabs;
